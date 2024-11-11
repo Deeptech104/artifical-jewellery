8c6c269f80b31f60532ce5efa143de8eff8151e2
Deeptech <deeptech104@gmail.com>
2024-11-11 22:53:00 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 // script.js
 console.log("Website Loaded");
+if (localStorage.getItem('cart')) {
+    cart = JSON.parse(localStorage.getItem('cart'));
+    updateCartCount();
+}
+
+// Function to update cart in localStorage
+function saveCart() {
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+// Modify addToCart to save cart to localStorage after adding an item
+function addToCart(productId) {
+    cart.push(productId);
+    updateCartCount();
+    saveCart(); // Save to localStorage
+}
